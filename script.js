38a313ca1bc99bd725b231c901cf33b6edad7e47
Oleg <olegivaniuk@gmail.com>
2020-10-19 15:59:31 +0300

    DZ number 1

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 "use strict";
-/*const user = "Oleg";
-alert('Privet, ' + user);*/
-let incr = 11,
-    decr = 11;
+//завдання на урок 1
+const numberOfFilms = +prompt(" Сколько фильмов вы уже посмотрели?", "");
 
-    console.log(++incr);
-    console.log(--decr);
+
+const personalMovieDB = { 
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+}; 
+
+const a = prompt("Один из последних просмотренных фильмов", ""),
+      b = prompt("На сколько оцените его", ""),
+      c = prompt("Один из последних просмотренных фильмов", ""),
+      d = prompt("На сколько оцените его", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
